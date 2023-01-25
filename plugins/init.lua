9a301170202effcc4f0b0de8160d2dfce78387fa
Finn Heemeyer <finn.heemeyer@comgy.io>
2023-01-25 14:45:09 +0100

    Add copilot

--- plugins/init.lua
+++ plugins/init.lua
@@ -30,10 +30,7 @@ return {
     run = "make"
   },
   ["andymass/vim-matchup"] = {
-    -- config = function()
-    --   require("vim-matchup").setup {
-    --     vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    --   }
-    -- end
+  },
+  ["github/copilot.vim"] = {
   },
 }
